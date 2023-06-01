a881036155eab7a34838c8e242be05060ef1fbb3
sangkyu.kim <doodllllllll@gmail.com>
2023-06-01 15:31:55 +0900

    [feature] 40p

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -19,3 +19,8 @@ TEST_F(PrimeFixture, TestName2) {
 	expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
+
+TEST_F(PrimeFixture, TestName3) {
+	expected = { 3 };
+	EXPECT_EQ(expected, prime_factor.of(3));
+}
