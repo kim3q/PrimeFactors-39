7ea89e4ba9e2ef4f6dc4a72e004be551f9da2292
sangkyu.kim <doodllllllll@gmail.com>
2023-06-01 15:29:27 +0900

    [feature] 37p

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -7,5 +7,10 @@ TEST(TestCaseName, TestName) {
 	PrimeFactor prime_factor;
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
+}
 
+TEST(TestCaseName, TestName2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
