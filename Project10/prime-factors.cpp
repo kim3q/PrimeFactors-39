21caba7832c339b832a9a56be48f38d23e62fb7e
sangkyu.kim <doodllllllll@gmail.com>
2023-06-01 15:41:50 +0900

    [refactoring] 52p

--- Project10/prime-factors.cpp
+++ Project10/prime-factors.cpp
@@ -19,18 +19,14 @@ public:
 			}
 			else if (number == 6)
 			{
-				while (number % divisor == 0)
+				for ( divisor = 2; number > 1; divisor++)
 				{
-					result.push_back(divisor);
-					number /= divisor;
+					while (number%divisor == 0)
+					{
+						result.push_back(divisor);
+						number /= divisor;
+					}
 				}
-				divisor++;
-				while (number % divisor == 0)
-				{
-					result.push_back(divisor);
-					number /= divisor;
-				}
-				divisor++;
 			}
 			else {
 				result.push_back(number);
